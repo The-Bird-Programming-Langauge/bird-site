24136dc5e5b0adb2a81505d19ffa5d62dcbb2574
Cole Carley <cole.carley@gmail.com>
2025-02-04 04:08:15 +0000

    trying without index.html

--- Frontend/svelte.config.js
+++ Frontend/svelte.config.js
@@ -1,6 +1,8 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -13,7 +15,13 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/bird-site' // Adjust for GitHub Pages
+		},
+		prerender: {
+			entries: ['*'] // Prerender all pages
+		}
 	}
 };
 
