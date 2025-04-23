0837719161448684b404e1633955a0bc9bee2ea6
nathanwrightUNR <nathanwright@nevada.unr.edu>
2025-04-23 21:17:12 +0000

    print multi-arg on single line

--- Frontend/src/lib/compile.ts
+++ Frontend/src/lib/compile.ts
@@ -91,24 +91,26 @@ export async function uploadWasm(code: Uint8Array | ArrayBuffer) {
         );
     }
 }
+
 class Printer {
+    private currentLine = "";
+
     print_i32(value: number) {
-        console.log(value);
-        consoleOutput.update(old => [...old, value.toString()]);
+        this.currentLine += value.toString();
     }
+
     print_char(value: number) {
-        const char = String.fromCharCode(value);
-        console.log(char);
-        consoleOutput.update(old => [...old, char]);
+        this.currentLine += String.fromCharCode(value);
     }
+
     print_f64(value: number) {
-        console.log(value);
-        consoleOutput.update(old => [...old, value.toString()]);
+        this.currentLine += value.toString();
     }
+
     print_bool(value: number) {
-        console.log(!!value);
-        consoleOutput.update(old => [...old, (!!value).toString()]);
+        this.currentLine += value === 1 ? "true" : "false";
     }
+
     print_str(ptr: number) {
         const ref = mem.get(ptr);
         const array_ptr = mem.get(ref.get_32(0));
@@ -120,13 +122,16 @@ class Printer {
             str += String.fromCharCode(data.get_32(i * 4));
         }
 
-        consoleOutput.update(old => [...old, str]);
-        console.log(str)
+        this.currentLine += str;
     }
+
     print_endline() {
-        console.log();
+        if (this.currentLine.length > 0) {
+            consoleOutput.update(old => [...old, this.currentLine]);
+            this.currentLine = "";
+        }
     }
-};
+}
 
 class Block {
     static SIZE_OFFSET = 4;
@@ -410,6 +415,7 @@ const moduleOptions = {
         push_64,
         strcat,
         strcmp,
+        str_to_array,
         print_i32: (value: number) => printer.print_i32(value),
         print_char: (value: number) => printer.print_char(value),
         print_f64: (value: number) => printer.print_f64(value),
@@ -436,6 +442,10 @@ const moduleOptions = {
         },
     }
 };
+
+function str_to_array(str_ptr: number) {
+    return str_ptr;
+}
 
 function push_ptr(arr_ptr: number, value: number) {
     const ref = mem.get(arr_ptr);
